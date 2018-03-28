8526098efce12c53e7f827f8ac88e26d870ce991
Trung Nguyen <trungnv184@gmail.com>
2018-03-28 22:46:33 +0700

    add new demo

---
+++ src/pages/datatypes/company.ts
@@ -0,0 +1,5 @@
+export class Company {
+  id: string;
+  name: string;
+  location: string;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,40 +1,69 @@
 import {Observable} from "rxjs/Rx";
 import {Component} from "@angular/core";
+import * as _ from "lodash";
+
 import {
-  ActionSheetController,
-  AlertController,
   NavController
 } from "ionic-angular";
 import {
-  AngularFireDatabase,
+  AngularFireDatabase, AngularFireList,
   AngularFireObject
 } from "angularfire2/database";
+import {Company} from "../datatypes/company";
+import {UUID} from "angular2-uuid";
 
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
 export class HomePage {
-  itemRef: AngularFireObject<any>;
-  item: Observable<any>;
+  itemRef: AngularFireObject<Company>;
+  companiesRef: AngularFireList<Company>;
 
   constructor(public navCtrl: NavController,
-              public alertCtrl: AlertController,
-              public db: AngularFireDatabase,
-              public actionSheetCtrl: ActionSheetController) {
-    this.itemRef = db.object('item');
+              public db: AngularFireDatabase,) {
+    this.itemRef = db.object('company');
+    this.companiesRef = db.list('companies');
   }
 
   addNewItem() {
-    this.itemRef.set({id: 1, name: 'NVG'});
+    let company: Company = new Company();
+    company.id = UUID.UUID();
+    company.name = `ANG_${_.random(100, 999)}`;
+    company.location = `${_.random(2000, 3000)}, CMT8, HCM`;
+    this.itemRef.set(company);
   }
 
   updateItem() {
-    this.itemRef.update({location: 'CMT8'})
+    this.itemRef.update({location: '222 - LTK - Q10'})
   }
 
   deleteItem() {
     this.itemRef.remove();
+  }
+
+  addItemList() {
+    let company: Company = new Company();
+    company.id = UUID.UUID();
+    company.name = `ANG_${_.random(100, 999)}`;
+    company.location = `${_.random(2000, 3000)}, CMT8, HCM`;
+    this.companiesRef.push(company).then((data) => {
+      console.log(data);
+    });
+  }
+
+  updateItemList() {
+    let company: Company = new Company();
+    company.id = UUID.UUID();
+    company.name = `ANG_${_.random(100, 999)}`;
+    company.location = `${_.random(2000, 3000)}, LTK`;
+
+    this.companiesRef.update('-L8gGt__Q4i_Wp0VdTLT', company);
+
+  }
+
+  deleteList() {
+    this.companiesRef.remove('-L8gGt__Q4i_Wp0VdTLT');
   }
 
 }
