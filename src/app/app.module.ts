568a44497797bad97c57a9b4da3b7aa7d6d73bdc
Trung Nguyen <trungnv184@gmail.com>
2018-03-28 11:29:39 +0700

    add demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,7 +8,19 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { HomePage } from "../pages/home/home";
+import { AngularFireModule } from 'angularfire2';
+
+
+export const firebaseConfiguration = {
+  apiKey: "AIzaSyB-wCQQFO_SSVNbaqcb4rObYLZ4o4nbUSM",
+  authDomain: "ionic-firebase-app-e5e6f.firebaseapp.com",
+  databaseURL: "https://ionic-firebase-app-e5e6f.firebaseio.com",
+  projectId: "ionic-firebase-app-e5e6f",
+  storageBucket: "",
+  messagingSenderId: "800467503199"
+};
+
 
 @NgModule({
   declarations: [
@@ -14,12 +29,14 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfiguration),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage 
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Observable} from "rxjs/Rx";
+import {Component} from "@angular/core";
+import {
+  ActionSheetController,
+  AlertController,
+  NavController
+} from "ionic-angular";
+import {
+  AngularFireDatabase,
+  AngularFireObject
+} from "angularfire2/database";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  itemRef: AngularFireObject<any>;
+  item: Observable<any>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController,
+              public db: AngularFireDatabase,
+              public actionSheetCtrl: ActionSheetController) {
+    this.itemRef = db.object('item');
+  }
 
+  addNewItem() {
+    this.itemRef.set({id: 1, name: 'NVG'});
+  }
+
+  updateItem() {
+    this.itemRef.update({location: 'CMT8'})
+  }
+
+  deleteItem() {
+    this.itemRef.remove();
   }
 
 }
